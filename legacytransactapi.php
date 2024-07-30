df33d8c3f98f2c973b69b85b0459e58436f2d76d
Muhammad Shahrukh <>
2024-07-30 13:59:24 +0500

    COMCL-635: Upgrade civix

--- legacytransactapi.php
+++ legacytransactapi.php
@@ -24,24 +24,6 @@ function legacytransactapi_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function legacytransactapi_civicrm_postInstall() {
-  _legacytransactapi_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function legacytransactapi_civicrm_uninstall() {
-  _legacytransactapi_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
@@ -49,33 +31,3 @@ function legacytransactapi_civicrm_uninstall() {
 function legacytransactapi_civicrm_enable() {
   _legacytransactapi_civix_civicrm_enable();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function legacytransactapi_civicrm_disable() {
-  _legacytransactapi_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function legacytransactapi_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _legacytransactapi_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function legacytransactapi_civicrm_entityTypes(&$entityTypes) {
-  _legacytransactapi_civix_civicrm_entityTypes($entityTypes);
-}
-
